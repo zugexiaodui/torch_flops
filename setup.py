c0a70d6693292d70c545f56e5063ad13346a7f9e
luyue <luyue163@126.com>
2023-10-26 21:23:38 +0800

    fix description in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     version=pkg_version,
     author="Yue Lu",
     author_email="luyue163@126.com",
-    description="A libiary for calcuating the FLOPs in the forward() process based on torch.fx",
+    description="A library for calculating the FLOPs in the forward() process based on torch.fx",
     long_description=open(readme_path, encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     url="https://github.com/zugexiaodui/torch_flops",
