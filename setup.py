6fdc6c2229ebe286a0e5b182e3b82d283439cba2
luyue <luyue163@126.com>
2023-10-26 21:17:29 +0800

    upload to pypi

--- setup.py
+++ setup.py
@@ -5,11 +5,15 @@ with open("./torch_flops/__init__.py") as f:
     for line in f.readlines():
         if line.startswith('__version__'):
             pkg_version = line.strip('\n').split('\'')[-2]
+readme_path = "README.md"
 setup(
     name="torch_flops",
     version=pkg_version,
     author="Yue Lu",
     author_email="luyue163@126.com",
     description="A libiary for calcuating the FLOPs in the forward() process based on torch.fx",
+    long_description=open(readme_path, encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
     url="https://github.com/zugexiaodui/torch_flops",
+    data_files=[readme_path]
 )
